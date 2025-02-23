b1aaa7e275d3ab5c080691b0e0d021e833748f6e
djmagic32 <garciawork3232@gmail.com>
2025-02-23 03:36:55 -0400

    add new token view conf

--- backend_ojeda/views.py
+++ backend_ojeda/views.py
@@ -1,12 +1,28 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+import logging
+
+# Configura el logger
+logger = logging.getLogger(__name__)
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        # Puedes añadir más claims si lo necesitas
         return token
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        try:
+            response = super().post(request, *args, **kwargs)
+            return response
+        except AuthenticationFailed as e:
+            logger.error(f"Authentication failed: {str(e)}")
+            return Response({"detail": str(e)}, status=401)
+        except Exception as e:
+            logger.error(f"Unexpected error: {str(e)}")
+            return Response({"detail": "An unexpected error occurred."}, status=400)
