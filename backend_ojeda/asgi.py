492d57f71657f3efd099909302bcac28142ac462
DjMagic32 <garciawork3232@gmail.com>
2025-06-23 13:18:24 -0400

    fix asgi

--- backend_ojeda/asgi.py
+++ backend_ojeda/asgi.py
@@ -6,23 +6,15 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-ASGI config for backend_ojeda project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
 
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import store.routing # We will create this file next
+import store.routing  # We will create this file next
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_ojeda.settings')
 
-# Get the default Django ASGI application to handle HTTP requests
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
